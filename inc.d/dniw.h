4f4b2814f89881d6e1115c1ab1e77a6da54dfebf
Simon-Euchner <euchner.se@gmail.com>
2024-12-24 20:00:46 +0100

    Fixed typo in file head

--- inc.d/dniw.h
+++ inc.d/dniw.h
@@ -4,10 +4,10 @@
  *                                                                            *
  * -------------------------------------------------------------------------- *
  *                                                                            *
- * LICENSES: GPL-3.0                                                          *
+ * LICENSE: GPL-3.0                                                           *
  *                                                                            *
- * IMPORTANT: THIS IS FREE SOFTWARE WITHOUT ANY WARRANTY. THE USER IS IF FREE *
- *            TO MODIFY AND REDISTRIBUTE THIS SOFTWARE UNDER THE TERMS OF THE *
+ * IMPORTANT: THIS IS FREE SOFTWARE WITHOUT ANY WARRANTY. THE USER IS FREE TO *
+ *            MODIFY AND REDISTRIBUTE THIS SOFTWARE UNDER THE TERMS OF THE    *
  *            LICENSE LISTED ABOVE PUBLISHED BY THE FREE SOFTWARE FOUNDATION. *
  *            THE PUBLISHER, SIMON EUCHNER, IS NOT RESPONSIBLE FOR ANY        *
  *            NEGATIVE EFFECTS THIS SOFTWARE MAY CAUSE.                       *
